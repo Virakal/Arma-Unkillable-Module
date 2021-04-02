7a1d0f072e890eeb7f1d4501fb46d75e3ba06d89
Jon Goodger <jonno.is@gmail.com>
2021-04-02 19:32:12 +0100

    Initial commit

---
+++ config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+    class Virakal_UnkillableModule {
+        requiredAddons[] = {"A3_Data_F"};
+        requiredVersion = 0.1;
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+class CfgInventoryGlobalVariable {
+    maxSoldierLoad = 10000;
+};
---
+++ mod.cpp
@@ -0,0 +1,14 @@
+/// Name of your mod
+name = "Unkillable Unit Module";
+/// Website URL, that can accessed from the mod data
+action = "https://taskforce79.com";
+/// Tool tip displayed when the mouse is left over, in the main menu of Arma 3
+tooltipOwned = "Makes a unit unkillable.";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] = {	0.23, 0.39, 0.30, 1 };
+/// Overview text, displayed from the extension menu
+overview = "Makes a unit unkillable.";
+/// Hide the extension name
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
